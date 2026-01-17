67a4420895907c06295bf24fa81a887ee4463aa6
libra1801 <161287197+libra1801@users.noreply.github.com>
2026-01-17 23:29:43 +0800

    Update keep.py

--- keep2notion/keep.py
+++ keep2notion/keep.py
@@ -42,7 +42,6 @@ def login():
     countryCode = os.getenv("COUNTRY_CODE","86")
     mobile = os.getenv("KEEP_MOBILE")
     password = os.getenv("KEEP_PASSWORD")
-    print(password)
     data = {"mobile": mobile, "password": password,"countryCode":countryCode}
     r = requests.post(LOGIN_API, headers=keep_headers, data=data)
     if r.ok:
